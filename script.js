6516b7bb19f6a8ead661a875239fa68f055fb50c
Gareth Robinson <gazz_1998@hotmail.co.uk>
2022-03-15 16:29:13 +0000

    calculator class constructor created

---
+++ script.js
@@ -0,0 +1,8 @@
+class Calculator{
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
+    }
+
+    
